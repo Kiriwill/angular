0a3a17911b4fcdd99bcc6ba3a4b921c0e23ab8c3
Willian Pacheco <willian.pacheco@solidaridad.org>
2021-03-01 20:12:03 -0300

    Aula1

---
+++ hello-world/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { OlaAngularComponent } from "./ola-angular/ola-angular.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OlaAngularComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
